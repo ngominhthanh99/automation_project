6f53bcb78f19c970170795e27cb7395dd898472a
KEV\KelvinKev <ngominhthanh99@gmail.com>
2025-07-26 18:02:36 +0700

    fix checkoutbtn

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from base.base_page import BasePage
 
 class CartPage(BasePage):
-    checkout_btn = (By.XPATH, "//button[text()='Checkout']")
+    checkout_btn = (By.XPATH, "//button[@id='checkout']")
 
     def click_checkout(self):
         self.click(self.checkout_btn)
