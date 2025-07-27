0cbbd1018a2004842cb8b5a6c89d45e2fef6da17
KEV\KelvinKev <ngominhthanh99@gmail.com>
2025-07-27 13:26:24 +0700

    fix cartpage

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -5,4 +5,5 @@ class CartPage(BasePage):
     checkout_btn = (By.XPATH, "//button[@id='checkout']")
 
     def click_checkout(self):
+        self.wait_for_element(self.checkout_btn)
         self.click(self.checkout_btn)
